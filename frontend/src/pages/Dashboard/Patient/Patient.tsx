48aa02d75f196c86d9417f9d95f4bd23b6aa0222
Adauto Dorta <adautodortafilho@gmail.com>
2025-11-22 14:38:12 -0300

    feature/task-12/Complemento da área médica de médico - visualização de prontuários de pacientes

--- frontend/src/pages/Dashboard/Patient/Patient.tsx
+++ frontend/src/pages/Dashboard/Patient/Patient.tsx
@@ -1,13 +1,31 @@
-import {CardInfoPatient} from "./components/CardInfoPatient";
+import {useQuery} from "@tanstack/react-query";
+
 import {CheckinSection} from "./components/CheckinSection";
 import {RecordsList} from "./components/RecordsList";
+import {CardInfoPatient} from "../../../components/custom/CardInfoPatient";
+
+import {ProfilesAPI} from "@/api/api";
+import {Separator} from "@/components/ui/separator";
+import {getFirstTwoNames} from "@/utils/functions";
 
 export const PatientDashboard = () => {
+  const {data, isPending} = useQuery({
+    queryKey: ["info-patient"],
+    queryFn: ProfilesAPI.getInfoMe,
+  });
+
   return (
     <div className="px-6 w-full flex justify-center mt-7 lg:items-center">
       <div className="w-full max-w-[369px] flex flex-col items-center lg:items-start lg:max-w-[954px]">
-        <CardInfoPatient />
+        <h2 className="text-2xl font-medium mb-4 w-full">
+          Bem vindo(a),
+          {" "}
+          {getFirstTwoNames(data?.full_name ?? "")}
+        </h2>
         <CheckinSection />
+        <Separator className="mb-5 mt-5" />
+        <CardInfoPatient data={data} isPending={isPending} />
+        <Separator className="mb-5 mt-5" />
         <RecordsList />
       </div>
     </div>
--- frontend/src/pages/PatientResume/PatientResumeContent.tsx
+++ frontend/src/pages/PatientResume/PatientResumeContent.tsx
@@ -1,7 +1,14 @@
 import {useQuery} from "@tanstack/react-query";
+import {Activity, User} from "lucide-react";
 import {useParams} from "react-router";
 
+import {RecordsList} from "./components/RecordList";
+import {CardInfoPatient} from "../../components/custom/CardInfoPatient";
+
 import {ProfilesAPI} from "@/api/api";
+import {Spinner} from "@/components/ui/spinner";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import {formatCPF} from "@/utils/functions";
 
 export const PatientResumeContent = () => {
   const {patientId} = useParams<{patientId: string}>();
@@ -12,27 +19,59 @@ export const PatientResumeContent = () => {
     enabled: !!patientId,
   });
 
-  if (isLoading) {
-    return <div>Carregando...</div>;
-  }
-
   return (
     <div className="px-6 w-full flex justify-center mt-7 lg:items-center">
-      <div className="w-full max-w-[369px] flex flex-col items-center lg:items-start lg:max-w-[954px]">
-        <span className="text-blue-600 font-semibold">
-          {patient?.full_name
-            .split(" ")
-            .map(n => n[0])
-            .slice(0, 2)
-            .join("")}
-        </span>
-        <h2 className="text-2xl font-medium mb-4 self-start">
-          Pacientes
-        </h2>
-        <div>
-          Informações do paciente
-          {patientId}
-        </div>
+      <div className="w-full max-w-[369px] flex flex-col items-start lg:max-w-[954px]">
+        {isLoading
+          ? (
+              <div>
+                <Spinner />
+              </div>
+            )
+          : (
+              <>
+                <div className="w-full flex flex-row gap-4">
+                  <div className="w-12 h-12 bg-blue-100 rounded-full flex items-center justify-center">
+                    <span className="text-blue-600 font-semibold">
+                      {patient?.full_name
+                        .split(" ")
+                        .map(n => n[0])
+                        .slice(0, 2)
+                        .join("")}
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <h2 className="text-xl font-medium self-start">
+                      {patient?.full_name}
+                    </h2>
+                    <span>
+                      CPF:
+                      {" "}
+                      {formatCPF(patient?.document_number ?? "")}
+                    </span>
+                  </div>
+                </div>
+
+                <Tabs className="mt-7 w-full" defaultValue="account">
+                  <TabsList className="gap-1 rounded-full">
+                    <TabsTrigger value="account" className="flex gap-4 rounded-full">
+                      <User />
+                      Dados Pessoais
+                    </TabsTrigger>
+                    <TabsTrigger value="records" className="flex gap-4">
+                      <Activity />
+                      Histórico Médico
+                    </TabsTrigger>
+                  </TabsList>
+                  <TabsContent value="account">
+                    <CardInfoPatient data={patient} isPending={isLoading} />
+                  </TabsContent>
+                  <TabsContent value="records">
+                    <RecordsList patientId={patientId ?? ""} />
+                  </TabsContent>
+                </Tabs>
+              </>
+            )}
       </div>
     </div>
   );
